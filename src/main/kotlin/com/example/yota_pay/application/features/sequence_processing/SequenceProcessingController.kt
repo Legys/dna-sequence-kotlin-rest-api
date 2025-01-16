179b40b8c7e61f012ccd8da3c6a2604183dbc7bc
Alex Dubrovin <legyssss@gmail.com>
2025-01-16 17:42:24 +0200

    feat: add controller

--- src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/SequenceProcessingController.kt
+++ src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/SequenceProcessingController.kt
@@ -1,7 +1,9 @@
 package com.example.yota_pay.application.features.sequence_processing
 
+import com.example.yota_pay.application.features.sequence_processing.request.GcContentRequest
 import com.example.yota_pay.application.features.sequence_processing.request.SequenceValidationRequest
 import com.example.yota_pay.application.features.sequence_processing.response.SequenceValidationResponse
+import com.example.yota_pay.application.features.sequence_processing.service.GcContentService
 import com.example.yota_pay.application.features.sequence_processing.service.SequenceValidationService
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.PostMapping
@@ -13,12 +15,18 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/sequence")
 class SequenceProcessingController(
     private val sequenceValidationService: SequenceValidationService,
+    private val gcContentService: GcContentService,
 ) {
     @PostMapping("/validate")
     fun validateSequence(
-        @RequestBody @Valid request: SequenceValidationRequest,
+        @Valid @RequestBody request: SequenceValidationRequest,
     ): SequenceValidationResponse {
         val isValid = sequenceValidationService.validateSequence(request.sequence)
         return SequenceValidationResponse(isValid)
     }
+
+    @PostMapping("/gc-content")
+    fun calculateGcContent(
+        @Valid @RequestBody request: GcContentRequest,
+    ) = gcContentService.calculateGcContent(request)
 }
