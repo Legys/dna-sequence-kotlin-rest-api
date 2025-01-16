183d3906e1b1d8384f81e424daa4396bccd16264
Alex Dubrovin <legyssss@gmail.com>
2025-01-16 13:03:47 +0200

    init

---
+++ src/main/kotlin/com/example/yota_pay/DnaSequenceAnalyzerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yota_pay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DnaSequenceAnalyzerApplication
+
+fun main(args: Array<String>) {
+	runApplication<DnaSequenceAnalyzerApplication>(*args)
+}
