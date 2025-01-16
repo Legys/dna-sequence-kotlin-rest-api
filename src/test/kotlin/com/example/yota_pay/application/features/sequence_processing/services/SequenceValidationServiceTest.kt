dc8b60afebff2428d4cc045677a8c6dc1073138b
Alex Dubrovin <legyssss@gmail.com>
2025-01-16 13:39:57 +0200

    refactor: Improve formatting and structure of SequenceValidationServiceTest

--- src/test/kotlin/com/example/yota_pay/application/features/sequence_processing/services/SequenceValidationServiceTest.kt
+++ src/test/kotlin/com/example/yota_pay/application/features/sequence_processing/services/SequenceValidationServiceTest.kt
@@ -1,31 +1,33 @@
 package com.example.yota_pay.application.features.sequence_processing.services
 
+import com.example.yota_pay.application.features.sequence_processing.service.SequenceValidationService
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.booleans.shouldBeFalse
 import io.kotest.matchers.booleans.shouldBeTrue
 
-class SequenceValidationServiceTest : BehaviorSpec({
+class SequenceValidationServiceTest :
+    BehaviorSpec({
 
-    val service = SequenceValidationService()
+        val service = SequenceValidationService()
 
-    given("a valid DNA sequence") {
-        `when`("validating the sequence") {
-            then("it should return true") {
-                service.validateSequence("ATCG").shouldBeTrue()
-                service.validateSequence("GCTA").shouldBeTrue()
-                service.validateSequence("AAAA").shouldBeTrue()
+        given("a valid DNA sequence") {
+            `when`("validating the sequence") {
+                then("it should return true") {
+                    service.validateSequence("ATCG").shouldBeTrue()
+                    service.validateSequence("GCTA").shouldBeTrue()
+                    service.validateSequence("AAAA").shouldBeTrue()
+                }
             }
         }
-    }
 
-    given("an invalid DNA sequence") {
-        `when`("validating the sequence") {
-            then("it should return false") {
-                service.validateSequence("ATCX").shouldBeFalse()
-                service.validateSequence("1234").shouldBeFalse()
-                service.validateSequence("ATCG ").shouldBeFalse()
-                service.validateSequence("").shouldBeFalse()
+        given("an invalid DNA sequence") {
+            `when`("validating the sequence") {
+                then("it should return false") {
+                    service.validateSequence("ATCX").shouldBeFalse()
+                    service.validateSequence("1234").shouldBeFalse()
+                    service.validateSequence("ATCG ").shouldBeFalse()
+                    service.validateSequence("").shouldBeFalse()
+                }
             }
         }
-    }
-})
+    })
