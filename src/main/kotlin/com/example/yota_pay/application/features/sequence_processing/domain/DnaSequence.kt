72f5f7c03a9e7dd878c70834b8311af736c3bc59
Alex Dubrovin <legyssss@gmail.com>
2025-01-16 23:52:15 +0200

    feat: Add validation and reverse complement to DnaSequence

--- src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/domain/DnaSequence.kt
+++ src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/domain/DnaSequence.kt
@@ -1,4 +1,17 @@
 package com.example.yota_pay.application.features.sequence_processing.domain
 
 @JvmInline
-value class DnaSequence(val value: String)
+value class DnaSequence(
+    val value: String,
+) {
+    init {
+        require(value.all { it in "ACGT" }) { "Invalid DNA sequence: $value" }
+    }
+
+    fun reverseComplement(): DnaSequence {
+        val complement = mapOf('A' to 'T', 'T' to 'A', 'C' to 'G', 'G' to 'C')
+        return DnaSequence(value.reversed().map { complement[it] }.joinToString(""))
+    }
+
+    override fun toString(): String = value
+}
