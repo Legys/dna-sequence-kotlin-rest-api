816646b006f8adcff3021fe449b0af5a4f80c60d
Alex Dubrovin <legyssss@gmail.com>
2025-01-16 13:29:30 +0200

    feat: Add SequenceProcessingController for sequence processing API

---
+++ src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/SequenceProcessingController.kt
@@ -0,0 +1,8 @@
+package com.example.yota_pay.application.features.sequence_processing
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/sequence")
+class SequenceProcessingController
