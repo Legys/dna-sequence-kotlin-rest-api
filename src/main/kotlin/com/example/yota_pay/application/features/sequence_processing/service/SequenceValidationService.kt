d5da1d16a0fc16bed801f267ed52e63676dbd964
Alex Dubrovin <legyssss@gmail.com>
2025-01-17 00:00:44 +0200

    refactor: Simplify validateSequence method in SequenceValidationService

--- src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/service/SequenceValidationService.kt
+++ src/main/kotlin/com/example/yota_pay/application/features/sequence_processing/service/SequenceValidationService.kt
@@ -1,10 +1,9 @@
 package com.example.yota_pay.application.features.sequence_processing.service
 
-import com.example.yota_pay.application.features.sequence_processing.domain.DnaSequence
 import com.example.yota_pay.application.features.sequence_processing.domain.SequenceValidator
 import org.springframework.stereotype.Service
 
 @Service
 class SequenceValidationService {
-    fun validateSequence(sequence: String) = SequenceValidator.validateSequence(sequence).isSuccess
+    fun validateSequence(sequence: String) = SequenceValidator.validateSequence(sequence)
 }
